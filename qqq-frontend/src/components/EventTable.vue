2a029d311cf1313448cb26080732561927d1170e
Jon Skulski <jskulski@gmail.com>
2019-05-24 02:18:55 -0700

    Clean up

--- qqq-frontend/src/components/EventTable.vue
+++ qqq-frontend/src/components/EventTable.vue
@@ -4,12 +4,14 @@
   <vuetable ref="vuetable"
     api-url="http://localhost:3600/events"
     pagination-path=""
-    data-path="mydata"
+    data-path="event_data"
     :fields="['uuid', 'recorded_at', 'message']"
   ></vuetable>
 </template>
 
 <script>
+
+
 import Vuetable from 'vuetable-2/src/components/Vuetable'
 
 export default {
@@ -18,31 +20,16 @@ export default {
   },
   methods: {
     transform: function transform(data) {
-      console.log(arguments);
 
-        var transformed = {}
+      var transformed = {}
 
-        // transformed.pagination = {
-        //   total: data.total,
-        //   per_page: data.per_page,
-        //   current_page: data.current_page,
-        //   last_page: data.last_page,
-        //   next_page_url: data.next_page_url,
-        //   prev_page_url: data.prev_page_url,
-        //   from: data.from,
-        //   to: data.to
-        // }
-
-      transformed.mydata = []
+      transformed.event_data = []
 
       for (var i=0; i < data.length; i++) {
-        transformed.mydata.push({
-          // id: data[i].id,
+        transformed.event_data.push({
           recorded_at: data[i].recorded_at,
           uuid: data[i].uuid,
           message: data[i].message,
-          // created_at: data[i].created_at,
-          // updated_at: data[i].updated_at,
         })
       }
 
