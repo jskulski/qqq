6d8086626c4206f0623e822d237459fa5b3e12a7
Jon Skulski <jskulski@gmail.com>
2019-05-25 01:04:30 -0700

    Moving api recording away for focus

--- qqq-gem/lib/qqq/api.rb
+++ qqq-gem/lib/qqq/api.rb
@@ -2,6 +2,15 @@ require 'faraday'
 
 module QQQ
   module API
+
+    # desc :send, "Sends message to a remote server"
+    # def send
+    #   QQQ.subscribe do |event|
+    #     puts "Remotely publishing: #{event.message}"
+    #     API.publish(event)
+    #   end
+    # end
+
     def self.publish(event)
       conn = Faraday.new(:url => "http://localhost:3600")
       resp = conn.post '/events', { :event => event.as_json }
